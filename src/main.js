6bf8557cdcd12d629c0cd22687ffcbcd59174702
Allea Ward <Allea_ward@hotmail.com>
2018-02-27 05:49:36 +1000

    Initialise a Node.js app using Webpack and Vue.

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import Home from './components/Home.vue'
+import Resume from './components/Resume.vue'
+import Portfolio from './components/Portfolio.vue'
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path:'/', component: Home },
+  { path: '/Resume', component: Resume },
+  { path: '/Portfolio', component: Portfolio }
+];
+
+const router = new VueRouter({
+  routes,
+  mode : 'history'
+});
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
