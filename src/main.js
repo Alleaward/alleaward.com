afae085566c050e1b5b6fb293b704e9658eb4e0f
Allea Ward <Allea_ward@hotmail.com>
2018-02-28 18:58:28 +1000

    Changed into page orientation

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import Home from './components/Home.vue'
+import Home2 from './components/Home2.vue'
 import Resume from './components/Resume.vue'
 import Portfolio from './components/Portfolio.vue'
 
@@ -9,6 +10,7 @@ Vue.use(VueRouter);
 
 const routes = [
   { path:'/', component: Home },
+  { path: '/Home2', component: Home2 },
   { path: '/Resume', component: Resume },
   { path: '/Portfolio', component: Portfolio }
 ];
