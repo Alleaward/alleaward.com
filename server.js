c3e23fceb1cf13f978d37f943c6df252a613d5da
alleaward <Allea_ward@hotmail.com>
2018-04-15 23:02:12 +1000

    Still working on getting the server to serve up the correct files.

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var path = require('path');
-app = express();
+var app = express();
 
-var history = require('connect-history-api-fallback');
+// var history = require('connect-history-api-fallback');
+// app.use(history({ 
+//     index: __dirname + "/dist/build.js",
+//     htmlAcceptHeaders: ['text/html', 'application/xhtml+xml'],
+//     verbose: true
+// })); 
 
-app.use(history(__dirname + "/dist/index.html")); // express app "uses" the middleware (history) which takes the argument '__dirname + "/dist"' which i think makes an absolute path.
+// app.get("/", (req,res) => {
+    // res.sendFile(__dirname + "/dist/build.js"); //Just shows literal contents of file
+    // res.render(__dirname + "/dist/build.js"); //Error: Cannot find module 'html'
+    // res.render(__dirname + "/dist/"); //Error: No default engine was specified and no extension was provided.
+    // res.render(__dirname + "/dist"); //Error: No default engine was specified and no extension was provided.
+    // res.sendFile(__dirname + "/index.html"); //Error: Cannot find module 'html'
+    // res.render(__dirname + "/index.html"); //Error: Cannot find module 'html'
+    // res.sendFile(path.resolve(__dirname, "dist/")); //Cannot GET /
+    // res.render(__dirname + "dist"); //Error: Cannot find module 'comdist'
+// });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port);
